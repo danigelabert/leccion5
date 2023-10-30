d52de52c5a0dc42bc21016e1423526ef487803d9
danigelabert <73782173+danigelabert@users.noreply.github.com>
2023-10-30 08:34:55 +0100

    Splash background FET

---
+++ app/src/main/java/com/danigelabert/leccion5/splashActivity.kt
@@ -0,0 +1,13 @@
+package com.danigelabert.leccion5
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+
+    }
+}
